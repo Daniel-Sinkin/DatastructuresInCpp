76fda6399a45d2b807c2eca9fed4a9b483ee3fc6
Daniel-Sinkin <danielsinkin97@gmail.com>
2025-11-10 01:22:08 +0100

    Implemented double linked list

---
+++ dsalgo/src/linked_list_double.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+struct LinkedListDoubleNode
+{
+    int value = 0;
+    LinkedListDoubleNode *prev = nullptr;
+    LinkedListDoubleNode *next = nullptr;
+};
+
+class LinkedListDouble
+{
+public:
+    LinkedListDouble(const LinkedListDouble &) = delete;
+    LinkedListDouble &operator=(const LinkedListDouble &) = delete;
+    LinkedListDouble(LinkedListDouble &&) noexcept = delete;
+    LinkedListDouble &operator=(LinkedListDouble &&) noexcept = delete;
+
+    ~LinkedListDouble();
+
+    bool is_empty() const noexcept;
+    void push_front(int v);
+    void pop_front() noexcept;
+    void push_back(int v);
+    void pop_back() noexcept;
+    void clear() noexcept;
+
+    LinkedListDoubleNode *front() const noexcept;
+    LinkedListDoubleNode *back() const noexcept;
+
+private:
+    LinkedListDoubleNode *m_head = nullptr;
+
+    LinkedListDoubleNode *get_tail() const noexcept;
+};
