74fd39aabf258300f32acac9666f34af662ef7e3
Daniel-Sinkin <danielsinkin97@gmail.com>
2025-11-10 12:00:10 +0100

    Continueing work on HashMap
    Will need to make List more generic to allow nested lists

--- dsalgo/src/list.hpp
+++ dsalgo/src/list.hpp
@@ -12,7 +12,7 @@ namespace dsalgo
 {
 
 template <class T>
-    requires std::is_trivially_copyable_v<T>
+// requires(std::is_trivially_copyable_v<T> || std::is_trivially_move_constructible_v<T>)
 class List
 {
 public:
@@ -121,6 +121,27 @@ public:
     [[nodiscard]] std::size_t get_capacity() const noexcept { return m_start ? static_cast<std::size_t>(m_capacity - m_start) : 0zu; }
     [[nodiscard]] bool is_empty() const noexcept { return m_end == m_start; }
     [[nodiscard]] bool is_full() const noexcept { return m_end == m_capacity; }
+    T *get_start() noexcept { return m_start; }
+    T *get_end() noexcept { return m_end; }
+    const T *get_start() const noexcept { return m_start; }
+    const T *get_end() const noexcept { return m_end; }
+
+    [[nodiscard]] T &operator[](std::size_t index) noexcept { return m_start[index]; }
+    [[nodiscard]] const T &operator[](std::size_t index) const noexcept { return m_start[index]; }
+
+    [[nodiscard]] T &at(std::size_t index)
+    {
+        if (index >= get_length())
+            throw std::out_of_range("List::at index out of range");
+        return m_start[index];
+    }
+
+    [[nodiscard]] const T &at(std::size_t index) const
+    {
+        if (index >= get_length())
+            throw std::out_of_range("List::at index out of range");
+        return m_start[index];
+    }
 
 private:
     T *m_start{};
--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <print>
 #include <vector>
 
+#include "list.hpp"
+
 using namespace dsalgo;
 
 constexpr size_t round_power_of_2_up(size_t v) { return std::bit_ceil(v == 0 ? 1zu : v); }
@@ -20,50 +22,50 @@ template <Hashable K, typename V>
 class HashMap
 {
 public:
-    HashMap(size_t n_buckets) : m_buckets(round_power_of_2_up(n_buckets)) {}
+    HashMap(size_t n_buckets)
+    {
+        const std::size_t buckets = round_power_of_2_up(n_buckets);
+        m_buckets.reserve(buckets);
+        for (std::size_t i = 0; i < buckets; ++i)
+            m_buckets.emplace_back(List<HashMapNode<K, V>>{});
+    }
     HashMap() : m_buckets(64zu) {}
 
     void pprint() const
     {
-        std::println("HashMap with {} buckets:", m_buckets.size());
-        for (size_t i = 0; i < m_buckets.size(); ++i)
+        std::println("HashMap with {} buckets:", m_buckets.get_length());
+        for (size_t i = 0; i < m_buckets.get_length(); ++i)
         {
+            const auto &bucket = m_buckets[i];
             std::print("  [{}] ", i);
-            if (m_buckets[i].empty())
+            if (m_buckets[i].is_empty())
             {
                 std::println("<empty>");
                 continue;
             }
-            for (const auto &node : m_buckets[i])
+            for (size_t j = 0; j < bucket.get_length(); ++j)
             {
+                const auto &node = bucket[j];
                 std::print("({}: {:.3f}) ", node.key, node.value);
             }
             std::println("");
         }
     }
 
-    size_t get_length() const noexcept { return m_buckets.size(); }
+    size_t get_length() const noexcept { return m_buckets.get_length(); }
     size_t get_mask() const noexcept { return static_cast<size_t>(get_length() - 1); }
 
     void insert(K key, V value)
     {
-        std::println("Inserting [{}] := {}", key, value);
         u64 hashed = hash_int(key);
-        std::println("Hash of key is {}", hashed);
         size_t bucket_id = hashed & get_mask();
-        std::println("BucketID is {}", bucket_id);
         m_buckets[bucket_id].emplace_back(key, value);
-        std::println("Pushed node [{},{}] into bucket {}", key, value, bucket_id);
     }
 
 private:
-    std::vector<std::vector<HashMapNode<K, V>>> m_buckets;
+    List<List<HashMapNode<K, V>>> m_buckets;
 };
 
 int main()
 {
-    HashMap<uint32_t, double> hm{5};
-    hm.pprint();
-    hm.insert(5, 3);
-    hm.pprint();
 }
