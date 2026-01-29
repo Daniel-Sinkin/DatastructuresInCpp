9b9d61f0b5818240caba981a21183a011cdf1abb
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-01-29 07:29:40 +0100

    Adjsuted build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # build.sh
 rm -rf build
 cmake -G Ninja -S . -B build \
-  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_BUILD_TYPE=Debug \
   -DCMAKE_CXX_COMPILER=/opt/homebrew/opt/llvm/bin/clang++
 cmake --build build -j8
